bfdb46b5903e63ddb6f070272526e96a491d7c77
Xkyrell <xkyrell@gmail.com>
2024-12-18 19:42:01 +0200

    Implement RemapProxyFactory#remap, #staticRemap and #constructRemap

--- src/main/java/me/xkyrell/ksremapper/proxy/ProxyInvocationMapper.java
+++ src/main/java/me/xkyrell/ksremapper/proxy/ProxyInvocationMapper.java
@@ -3,6 +3,7 @@ package me.xkyrell.ksremapper.proxy;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import me.xkyrell.ksremapper.RemapProxy;
+import java.lang.invoke.MethodHandle;
 
 @Getter
 @RequiredArgsConstructor
@@ -12,4 +13,8 @@ public class ProxyInvocationMapper {
     private final Class<?> handleClass;
 
     // TODO: search for a class and its members
+
+    public MethodHandle findConstructor(Class<?>[] unwrappedTypes) {
+        throw new UnsupportedOperationException("Method findConstructor is not implemented");
+    }
 }
--- src/main/java/me/xkyrell/ksremapper/proxy/RemapProxyFactory.java
+++ src/main/java/me/xkyrell/ksremapper/proxy/RemapProxyFactory.java
@@ -1,22 +1,100 @@
 package me.xkyrell.ksremapper.proxy;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.google.common.reflect.Reflection;
 import lombok.NonNull;
 import me.xkyrell.ksremapper.RemapProxy;
+import javax.annotation.Nullable;
+import java.lang.invoke.MethodHandle;
 import java.lang.reflect.InvocationHandler;
 
 public class RemapProxyFactory {
 
+    private final LoadingCache<Class<? extends RemapProxy>, ProxyInvocationMapper> mappers =
+            CacheBuilder.newBuilder().build(CacheLoader.from(this::loadMapper));
+
     public final <T extends RemapProxy> T remap(@NonNull Class<T> clazz, @NonNull Object handle) {
-        return null;
+        ProxyInvocationMapper mapper = mappers.getUnchecked(clazz);
+
+        Class<?> handleClass = mapper.getHandleClass();
+        Class<?> actualClass = handle.getClass();
+        if (!handleClass.isAssignableFrom(actualClass)) {
+            throw new IllegalStateException(String.format(
+                    "Incorrect object type expected %s but got %s",
+                    handleClass.getName(), actualClass.getName())
+            );
+        }
+
+        return createProxy(clazz, mapper, handle);
     }
 
     public final <T extends RemapProxy> T staticRemap(@NonNull Class<T> clazz) {
-        return null;
+        ProxyInvocationMapper mapper = mappers.getUnchecked(clazz);
+        return createProxy(clazz, mapper, null);
     }
 
     public final <T extends RemapProxy> T constructRemap(@NonNull Class<T> clazz, @NonNull Object... params) {
-        return null;
+        Object[] unwrappedArgs = unwrapArguments(params);
+        ProxyInvocationMapper mapper = mappers.getUnchecked(clazz);
+        MethodHandle constructor = mapper.findConstructor(
+                getParameterTypes(unwrappedArgs)
+        );
+
+        Object instance = null;
+        try {
+            instance = constructor.invokeWithArguments(unwrappedArgs);
+        }
+        catch (Throwable ex) {
+            throw new RuntimeException(String.format(
+                    "Failed to construct remap proxy for class '%s': %s",
+                    clazz.getName(), ex
+            ));
+        }
+        return remap(clazz, instance);
+    }
+
+    Class<?>[] getParameterTypes(Object[] unwrappedArgs) {
+        if (unwrappedArgs == null || unwrappedArgs.length == 0) {
+            return new Class[0];
+        }
+
+        Class<?>[] result = new Class[unwrappedArgs.length];
+        for (int i = 0; i < unwrappedArgs.length; i++) {
+            result[i] = unwrappedArgs[i].getClass();
+        }
+        return result;
+    }
+
+    Object[] unwrapArguments(Object[] args) {
+        if (args == null) {
+            return new Object[0];
+        }
+
+        if (args.length == 0) {
+            return args;
+        }
+
+        Object[] result = new Object[args.length];
+        for (int i = 0; i < args.length; i++) {
+            result[i] = unwrapArgument(args[i]);
+        }
+        return result;
+    }
+
+    @Nullable
+    Object unwrapArgument(Object arg) {
+        if (arg instanceof RemapProxy) {
+            return mappers.getUnchecked(((RemapProxy) arg)
+                    .getProxyClass())
+                    .getHandleClass();
+        }
+        return arg;
+    }
+
+    protected ProxyInvocationMapper loadMapper(Class<? extends RemapProxy> clazz) {
+        throw new UnsupportedOperationException("Method loadMapper is not implemented");
     }
 
     private <T extends RemapProxy> T createProxy(Class<T> clazz, ProxyInvocationMapper mapper, Object handle) {
